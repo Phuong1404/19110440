be62c6083f2825c44aa0e9c7d9bfad42fa7455b7
Phuong1404 <t.xuanphuong1404@gmail.com>
2022-09-27 21:52:09 +0700

    upload1

--- Models/student.model.js
+++ Models/student.model.js
@@ -1,32 +1,32 @@
 const myGroup = [
-    { mssv: '19110440', ten: 'Trịnh Xuân Phương' }
+    { id: '19110440', name: 'Trịnh Xuân Phương' }
 ]
 
 async function GetAll() {
     return myGroup
 }
 async function GetOne(id) {
-    let data = myGroup.find(val => val.mssv == id)
+    let data = myGroup.find(val => val.id == id)
     if (data) {
         return data
     }
     return -1
 }
 async function Create(req, id) {
-    let data = myGroup.find(val => val.mssv == id)
+    let data = myGroup.find(val => val.id== id)
     if (!data) {
-        myGroup.push({ mssv: req.mssv, ten: req.ten })
+        myGroup.push({ id: req.id, name: req.name })
         return 1
     }
     return -1
 }
 async function GetAllMessage() {
-    let data=`<html><body><ul>${myGroup.map((val)=>{return `<li>${val.ten}</li>`})}</ul></body></html>`
+    let data=`<html><body><ul>${myGroup.map((val)=>{return `<li>${val.name}</li>`})}</ul></body></html>`
     console.log(data)
     return data
 }
 async function GetMessage(id) {
-    let data = myGroup.find(val => val.mssv == id)
+    let data = myGroup.find(val => val.id == id)
     if (data) {
         return `<html><body><ul><li>${data.ten}</li></ul></body></html>`
     }
